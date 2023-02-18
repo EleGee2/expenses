bcd58abe15888fba2257bf0d5f73b9af632b787e
Jeromekene <mckenzieelegeonye@gmail.com>
2023-02-18 15:56:53 +0100

    update settings.py

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -134,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
